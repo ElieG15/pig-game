15ee370ec364a9b9423ddb7f33995c844d0dc199
elieg15 <elieghomsi@gmail.com>
2022-10-01 20:32:37 -0500

    Setting up the player turn with color by using 'toggle', also activate buttons hold, get rid of the dice once the player win and make the buttons inactive once the player win

--- script.js
+++ script.js
@@ -1,12 +1,11 @@
 'use strict';
 //El stand for element, and selecting elements
+const player0El = document.querySelector('.player--0');
+const player1El = document.querySelector('.player--1');
 const score0El = document.querySelector('#score--0');
 const score1El = document.getElementById('score--1'); //this is same like the one above, and getEltById don't have to use # anymore
 const current0El = document.getElementById('current--0');
 const current1El = document.getElementById('current--1');
-
-// const player0El = document.getElementById('player--0');
-// const player1El = document.getElementById('player--1');
 
 const diceEl = document.querySelector('.dice');
 const btnNew = document.querySelector('.btn--new');
@@ -18,40 +17,70 @@ score0El.textContent = 0;
 score1El.textContent = 0;
 diceEl.classList.add('hidden');
 
+const score = [0, 0];
 let currentScore = 0;
-let isCurrentPlayer1 = true;
+let activePlayer = 0;
+let playing = true;
+
+const switchPlayer = function () {
+  document.getElementById(`current--${activePlayer}`).textContent = 0;
+  activePlayer = activePlayer === 0 ? 1 : 0;
+  currentScore = 0;
+  player0El.classList.toggle('player--active');
+  player1El.classList.toggle('player--active');
+};
 
 //Rolling dice functionality
 btnRoll.addEventListener('click', function () {
-  //1) Generating a random dice roll
-  const dice = Math.trunc(Math.random() * 6) + 1;
-  console.log(dice);
+  if (playing) {
+    //1) Generating a random dice roll
+    const dice = Math.trunc(Math.random() * 6) + 1;
+    console.log(dice);
 
-  //2) Display dice
-  diceEl.classList.remove('hidden');
-  diceEl.src = `dice-${dice}.png`;
+    //2) Display dice
+    diceEl.classList.remove('hidden');
+    diceEl.src = `dice-${dice}.png`;
 
-  //3) Check for rolled 1: if true,
-  if (dice !== 1) {
-    console.log('not one');
-    // Add dice to current score
-    currentScore += dice; //which can be write also like currentScore = currentScore + 1
-    // current0El.textContent = currentScore; //change later
-    if (isCurrentPlayer1) {
-      current0El.textContent = currentScore;
+    //3) Check for rolled 1: if true,
+    if (dice !== 1) {
+      // Add dice to current score
+      currentScore += dice; //which can be write also like currentScore = currentScore + 1
+      document.getElementById(`current--${activePlayer}`).textContent =
+        currentScore;
     } else {
-      current1El.textContent = currentScore;
+      //switch to the next player
+      // document.getElementById(`current--${activePlayer}`).textContent = 0;
+      // activePlayer = activePlayer === 0 ? 1 : 0;
+      // currentScore = 0;
+      // player0El.classList.toggle('player--active');
+      // player1El.classList.toggle('player--active');
+      switchPlayer();
     }
-  } else {
-    //switch to the next player
-    console.log('one');
+  }
+});
 
-    if (isCurrentPlayer1) {
-      current0El.textContent = 0;
+btnHold.addEventListener('click', function () {
+  if (playing) {
+    //1) add current score to active place's score
+    score[activePlayer] += currentScore; //in order hand it mean score[1] = score[1] + currentScore
+    document.getElementById(`score--${activePlayer}`).textContent =
+      score[activePlayer];
+    //2a) check if player's score is >= 100,
+    if (score[activePlayer] >= 20) {
+      //b) finish the game
+      playing = false;
+      diceEl.classList.add('hidden');
+      document
+        .querySelector(`.player--${activePlayer}`)
+        .classList.add('player--winner');
+      document
+        .querySelector(`.player--${activePlayer}`)
+        .classList.remove('player--active');
     } else {
-      current1El.textContent = 0;
+      switchPlayer();
+
+      //3) switch to the next player
+      switchPlayer();
     }
-    isCurrentPlayer1 = !isCurrentPlayer1;
-    currentScore = 0;
   }
 });
